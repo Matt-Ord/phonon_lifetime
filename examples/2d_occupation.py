0755ca903d71cd5d2d52d35f10c2081ffc9ce9a9
Matthew Ord <matthew.ord1@gmail.com>
2026-05-29 12:42:04 +0000

    fix as_pristine_gamma_phonons

--- examples/2d_occupation.py
+++ examples/2d_occupation.py
@@ -14,7 +14,7 @@ from phonon_lifetime.lifetimes import (
 )
 from phonon_lifetime.phonon import (
     animate_phonon_xy,
-    get_gamma_phonons,
+    get_mesh_phonons,
 )
 from phonon_lifetime.system import build as build_system
 
@@ -23,28 +23,28 @@ if __name__ == "__main__":
     system = build_system.with_nearest_neighbor_forces(
         SuperCell(cell, n_repeats=(15, 15, 1)),
         spring_constant=1.0,
-        periodic=(True, False, False),
+        periodic=(True, True, False),
         threshold=(0.0, 1.1),
     ) + build_system.with_nearest_neighbor_forces(
         SuperCell(cell, n_repeats=(15, 15, 1)),
         spring_constant=0.5,
-        periodic=(True, False, False),
-        threshold=(1.1, 2.1),
+        periodic=(True, True, False),
+        threshold=(1.1, 1.9),
     )
-    # TODO: use mesh phonons here to achieve this??
-    locations = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45]
+
+    locations = [0]
     symmetry_broken_system = with_mass_defect(
         pristine=system,
         defects=MassDefect(defects=[(None, 10.01, location) for location in locations]),
     )
-    pristine_phonons = get_gamma_phonons(symmetry_broken_system)
+    pristine_phonons = get_mesh_phonons(symmetry_broken_system, (3, 3, 1))
     pristine_phonons = as_pristine_gamma_phonons(pristine_phonons)
-    mode_idx = pristine_phonons.get_mode_idx(branch=280, iq=0)
+    mode_idx = np.argsort(pristine_phonons.omega)[280]
 
     fig, ax, anim = animate_phonon_xy(pristine_phonons[mode_idx])
-    ax.set_title("Phonon Mode for 1D Chain with Vacancy Defect")
+    ax.set_title("Phonon Mode with Broken Symmetry")
     anim.save(
-        "./examples/figures/1d_chain.vacancy_defect.mode_animation1.gif",
+        "./examples/figures/2d_occupation.mode_animation.symmetry_broken.gif",
         dpi=300,
         writer="pillow",
     )
@@ -59,7 +59,7 @@ if __name__ == "__main__":
                 defects=[(None, mass, location) for location in locations]
             ),
         )
-        defect_phonons = get_gamma_phonons(defect)
+        defect_phonons = get_mesh_phonons(defect, (3, 3, 1))
         defect_phonons = as_pristine_gamma_phonons(defect_phonons)
 
         _, _, line = plot_survival_probability(
@@ -69,10 +69,10 @@ if __name__ == "__main__":
 
     ax.legend()
     ax.set_ylim(0, 1.05)
-    fig.savefig("./examples/figures/survival.against_mass.png", dpi=300)
+    fig.savefig("./examples/figures/2d_occupation.survival.against_mass.png", dpi=300)
 
     fig, ax, line = plot_first_order_scatter(
         pristine_phonons, defect_phonons, pristine_idx=mode_idx
     )
     ax.set_title("First-order Scattering of Defect Modes")
-    fig.savefig("./examples/figures/survival.first_order_scatter.png", dpi=300)
+    fig.savefig("./examples/figures/2d_occupation.first_order_scatter.png", dpi=300)
--- tests/vectors_test.py
+++ tests/vectors_test.py
@@ -2,16 +2,20 @@ import numpy as np
 
 from phonon_lifetime.cell import SuperCell
 from phonon_lifetime.cell import build as build_cell
-from phonon_lifetime.defect import MassDefect, with_mass_defect
+from phonon_lifetime.defect import (
+    MassDefect,
+    as_pristine_gamma_phonons,
+    with_mass_defect,
+)
 from phonon_lifetime.phonon import as_gamma_phonons, get_gamma_phonons, get_mesh_phonons
-from phonon_lifetime.system import with_nearest_neighbor_forces, with_zero_forces
+from phonon_lifetime.system import build as build_system
 
 
 def test_zero_mass_defect_vectors() -> None:
     cell = build_cell.cubic(mass=10, distance=1, structure="simple")
     cell = SuperCell(cell, n_repeats=(7, 1, 1))
 
-    system = with_zero_forces(cell)
+    system = build_system.with_zero_forces(cell)
     defect = with_mass_defect(pristine=system, defects=MassDefect(defects=[]))
 
     modes = get_gamma_phonons(defect)
@@ -21,11 +25,42 @@ def test_zero_mass_defect_vectors() -> None:
         np.testing.assert_array_equal(vectors[i].reshape(-1, 3), modes[i].vector)
 
 
+def test_as_pristine_gamma_phonons_no_defect() -> None:
+    cell = build_cell.cubic(mass=10, distance=1.0, structure="simple")
+    system = build_system.with_nearest_neighbor_forces(
+        SuperCell(cell, n_repeats=(5, 1, 1)),
+        spring_constant=1.0,
+        periodic=(True, False, False),
+        threshold=(0.9, 1.1),
+    ) + build_system.with_nearest_neighbor_forces(
+        SuperCell(cell, n_repeats=(5, 1, 1)),
+        spring_constant=0.5,
+        periodic=(True, False, False),
+        threshold=(1.9, 2.1),
+    )
+
+    symmetry_broken_system = with_mass_defect(
+        pristine=system,
+        defects=MassDefect(defects=[]),
+    )
+    defect_phonons = get_mesh_phonons(symmetry_broken_system, (33, 1, 1))
+    defect_phonons = as_pristine_gamma_phonons(defect_phonons)
+
+    pristine_phonons = get_mesh_phonons(system, n_repeats=(33, 1, 1))
+    pristine_phonons = as_gamma_phonons(pristine_phonons)
+
+    np.testing.assert_array_equal(
+        pristine_phonons.system.cell, defect_phonons.system.cell
+    )
+    np.testing.assert_array_equal(pristine_phonons.omega, defect_phonons.omega)
+    np.testing.assert_array_equal(pristine_phonons.vectors, defect_phonons.vectors)
+
+
 def test_vectors_orthogonality() -> None:
     cell = build_cell.cubic(mass=10, distance=1, structure="simple")
     cell = SuperCell(cell, n_repeats=(7, 1, 1))
 
-    system = with_nearest_neighbor_forces(cell, spring_constant=1.0)
+    system = build_system.with_nearest_neighbor_forces(cell, spring_constant=1.0)
 
     modes = get_mesh_phonons(system, n_repeats=(7, 1, 1))
     modes = as_gamma_phonons(modes)
